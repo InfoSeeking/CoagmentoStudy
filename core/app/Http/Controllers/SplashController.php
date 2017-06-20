3a18c46f124be70cbd8f2cf1e988eaf81f6cf4b9
mmitsui <mmitsui@scarletmail.rutgers.edu>
2017-06-20 17:04:13 -0400

    Edited splash page and splash redirect
    If already logged in, go to stage.  Otherwise, go to log
    in/registration page

--- core/app/Http/Controllers/SplashController.php
+++ core/app/Http/Controllers/SplashController.php
@@ -6,6 +6,7 @@ use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Validator;
+use Auth;
 
 use App\Utilities\Status;
 use App\Models\V2Notification;
@@ -13,7 +14,16 @@ use App\Models\V2Notification;
 class SplashController extends Controller
 {
     public function index() {
-        return view('splash');
+        if(Auth::check()){
+            return view('workspace.panel', [
+                'user' => Auth::user(),
+                'projectCount' => 0,
+                'sharedProjectCount' => 0
+            ]);
+        }else{
+            return view('splash');
+        }
+
     }
     public function notify(Request $req) {
         $validator = Validator::make($req->all(), [
