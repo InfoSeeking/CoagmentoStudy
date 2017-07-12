ba048e2dcf12485d0dea23174fb874bb13a3577f
RajviM7 <rajvi.m7@gmail.com>
2017-07-12 09:00:03 -0400

    columns added to questionnaire3
    TODO: Fix bug for migration of questionnaire3 columns

---
+++ core/app/Http/Controllers/questionnaire3_test_controller.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\StageProgressController;
+use App\Questionnaire2Test;
+
+class questionnaire2_test_controller extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+//        dd(request() -> all ());
+        
+        $post = new Questionnaire2Test;
+
+        //validaiton:
+        $this -> validate($request, [
+            'collegeYear' => 'required',
+        ]);
+
+        $post -> collegeYear = request("collegeYear");
+
+        //add here
+
+        $post -> save();
+
+        return redirect('/stages/next');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- core/database/migrations/2017_07_12_125321_add_columns_to_questionnaire3_tests_table.php
+++ core/database/migrations/2017_07_12_125321_add_columns_to_questionnaire3_tests_table.php
@@ -3,24 +3,19 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnsToQuestionnaire2TestsTable extends Migration {
+class AddColumnsToQuestionnaire3TestsTable extends Migration
+{
     /**
      * Run the migrations.
      *
      * @return void
      */
-    public function up() {
-        Schema::table('questionnaire2_tests', function (Blueprint $table) {
+    public function up()
+    {
+        Schema::table('questionnaire3_tests', function (Blueprint $table) {
             $table->integer('project_id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->integer('stages_id')->unsigned();
-            //date
-            //time
-            //timestamp
-            $table->timestamp('added_on');
-            //local_date
-            //local_time
-            //local_timestamp
         });
     }
 
@@ -31,11 +26,10 @@ class AddColumnsToQuestionnaire2TestsTable extends Migration {
      */
     public function down()
     {
-        Schema::table('questionnaire2_tests', function (Blueprint $table) {
+        Schema::table('questionnaire3_tests', function (Blueprint $table) {
             $table->dropColumn('project_id');
             $table->dropColumn('user_id');
             $table->dropColumn('stages_id');
-            $table->dropColumn('');
         });
     }
 }
